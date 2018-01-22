db8943c254e06f3c9c78febf6fe8605cd8303d22
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-22 15:02:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+	programmer_hash[:grace_hopper]
 
 end
 
@@ -57,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
